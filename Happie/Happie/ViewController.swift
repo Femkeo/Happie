f27de9586f5b5930f223bbc622240b492a1dd7ba
Femkeo <femke_offringa@hotmail.com>
2017-05-29 14:30:23 +0200

    Created Xcode file

---
+++ Happie/Happie/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Happie
+//
+//  Created by G.F Offringa on 29-05-17.
+//  Copyright © 2017 femkeo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
