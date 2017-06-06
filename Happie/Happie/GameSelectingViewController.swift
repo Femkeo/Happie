75135da4d1127c4fc63ddb51f29073b9e94b1e3e
Femkeo <femke_offringa@hotmail.com>
2017-06-06 11:08:22 +0200

    created functions for better readablity of the playable games

--- Happie/Happie/GameSelectingViewController.swift
+++ Happie/Happie/GameSelectingViewController.swift
@@ -16,6 +16,7 @@ class GameSelectingViewController: UIViewController {
     @IBOutlet var thirdGameButtons: [UIButton]!
     @IBOutlet var allButtons: [UIButton]!
     
+    
 //    var categoryOne = ["Game1.0", "Game1.1", "Game1.2", "Game2.0", "Game2.1", "Game2.2", "Game3.0", "Game3.1", "Game3.2"]
 //    var categoryTwo = ["Game4.0", "Game4.1", "Game4.2", "Game5.0", "Game5.1", "Game5.2", "Game6.0", "Game6.1", "Game6.2"]
 //    var categoryThree = ["Game7.0", "Game7.1", "Game7.2", "Game8.0", "Game8.1", "Game8.2", "Game9.0", "Game9.1", "Game9.2"]
@@ -24,11 +25,17 @@ class GameSelectingViewController: UIViewController {
     var PreviousButtonTitle = ""
     let userData = UserDefaults.standard.dictionary(forKey: "Games") as! [String : [String : [String : [String : Any]]]]
     var categorieArray = [String]()
+    var gameAmount: Int?
+    
+    var difficultyNumber = 0
+    var difficultyArray = ["easy", "medium", "hard"]
+
     
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        //this uses the info of the button on the previous page to correct
         gettingCorrectInfo(category: PreviousButtonTitle)
     }
     
@@ -36,7 +43,7 @@ class GameSelectingViewController: UIViewController {
     
     func gettingCorrectInfo(category: String){
         let categoryUserData = userData["Categories"] ?? Dictionary()
-        
+        //this collects the names of the games that match each category
         switch category{
             case "Droom het" : categorieArray = Array(categoryUserData["Droom het"]!.keys)
             fillingCurrentLabel()
@@ -53,23 +60,22 @@ class GameSelectingViewController: UIViewController {
 
             
             //creating a number so we can count through the arrays of difficulties and categories
-            var difficultyNumber = 0
+    
             //this is the array of difficulties
-            var difficultyArray = ["easy", "medium", "hard"]
             //for each label do something
             for i in 0..<allButtons.count{
                 //check if the difficulty that is used is true or false.
                 let checkingBool = userData["Categories"]![PreviousButtonTitle]![categorieArray[difficultyNumber]]![difficultyArray[difficultyNumber]]! as! Bool
+                
                 //if its true set it to active
                 if checkingBool == true{
-                    allButtons[i].setTitle(difficultyArray[difficultyNumber], for: .normal)
-                    allButtons[i].setTitleColor(UIColor.green, for: .normal)
-                    allButtons[i].isEnabled = true
+                    //First i is a placeholder for the function. Second i is in "for i in 0..<allbuttons.count"
+                    gameStateIsTrue(i: i)
+                    
                     //if its false set it to inactive
                 }else{
-                    allButtons[i].setTitle(difficultyArray[difficultyNumber], for: .normal)
-                    allButtons[i].setTitleColor(UIColor.gray, for: .normal)
-                    allButtons[i].isEnabled = false
+                    //First i is a placeholder for the function. Second i is in "for i in 0..<allbuttons.count"
+                    gameStateIsFalse(i: i)
                 }
                 //start over if 2 was reached (for the arrays)
                 if difficultyNumber == 2{
@@ -79,6 +85,20 @@ class GameSelectingViewController: UIViewController {
                 }
             }
         }
+    
+    // if the game is playable
+    func gameStateIsTrue(i: Int){
+        allButtons[i].setTitle(difficultyArray[difficultyNumber], for: .normal)
+        allButtons[i].setTitleColor(UIColor.green, for: .normal)
+        allButtons[i].isEnabled = true
+    }
+    
+    //if the game is not playable
+    func gameStateIsFalse(i: Int){
+        allButtons[i].setTitle(difficultyArray[difficultyNumber], for: .normal)
+        allButtons[i].setTitleColor(UIColor.gray, for: .normal)
+        allButtons[i].isEnabled = false
+    }
 
 
 }
