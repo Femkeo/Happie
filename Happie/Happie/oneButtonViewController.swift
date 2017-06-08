369b6d9589aa62e062538429c870b22934f2f0c2
GerbrichJ <jong1361@student.nhl.nl>
2017-06-08 16:06:11 +0200

    started design game-info page

--- Happie/Happie/oneButtonViewController.swift
+++ Happie/Happie/oneButtonViewController.swift
@@ -15,6 +15,7 @@ class oneButtonViewController: UIViewController, UIImagePickerControllerDelegate
     @IBOutlet weak var photoImage: UIImageView!
     @IBOutlet weak var descriptionLabel: UILabel!
     @IBOutlet weak var saveButton: UIBarButtonItem!
+    @IBOutlet weak var descriptionView: UIView!
     
     var difficultyArray = ["easy", "medium", "hard"]
     var gameFromPrevious = ""
@@ -43,10 +44,21 @@ class oneButtonViewController: UIViewController, UIImagePickerControllerDelegate
         saveButton.tintColor = UIColor.clear
         saveButton.isEnabled = false
         
-        startButton.layer.cornerRadius = 7.0
-        startButton.layer.borderWidth = 2.0
+        startButton.layer.cornerRadius = 20.0
+        startButton.layer.borderWidth = 1.5
         startButton.layer.borderColor = UIColor.black.cgColor
+        
+        startButton.titleLabel?.adjustsFontSizeToFitWidth = true
+        startButton.titleLabel?.minimumScaleFactor = 0.2
+        startButton.titleLabel?.numberOfLines = 50
+        
+        descriptionView.layer.cornerRadius = 20.0
+        
+        descriptionLabel.adjustsFontSizeToFitWidth = true
+        descriptionLabel.minimumScaleFactor = 0.2
+        descriptionLabel.numberOfLines = 50
 
+        
     }
     
     @IBAction func startAction(_ sender: Any) {
@@ -98,7 +110,7 @@ class oneButtonViewController: UIViewController, UIImagePickerControllerDelegate
     
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingImage image: UIImage!, editingInfo: [NSObject : AnyObject]!) {
-        photoImage.image = image
+//        photoImage.image = image
         saveButton.isEnabled = true
         self.dismiss(animated: true, completion: nil);
     }
@@ -107,41 +119,41 @@ class oneButtonViewController: UIViewController, UIImagePickerControllerDelegate
         switch gameFromPrevious{
         case "Raad mijn droom":
             descriptionLabel.text = "Denk je dat ‘Wie ben ik’ lastig is? Probeer dan eens ‘Wat droom ik?’, voor wie van een echte uitdagingen houdt!"
-            photoImage.image = UIImage(named: "Dreamguess")
+//            photoImage.image = UIImage(named: "Dreamguess")
             
         case "Wat is mijn karakter":
             descriptionLabel.text = " Vraag iemand om 5 karaktereigenschappen voor jou uit te kiezen. Kijk ook gerust of dat overeenkomt met wat je zelf zou invullen!"
-            photoImage.image = UIImage(named: "Kwaliteiten guess")
+//            photoImage.image = UIImage(named: "Kwaliteiten guess")
             
         case "Quick quiz":
             descriptionLabel.text = "Je hebt 20 seconden de tijd om zoveel mogelijk vragen te beantwoorden. Tik tak tik tak, tempo."
-            photoImage.image = UIImage(named: "QuickQuiz")
-            
+//            photoImage.image = UIImage(named: "QuickQuiz")
+//            
         case "Kwaliteiten quiz":
             descriptionLabel.text = "Check met deze quiz of je partner-in-crime dezelfde kwaliteiten in jou ziet als dat je dat zelf doet."
-            photoImage.image = UIImage(named: "KwaliteitenQuiz")
+//            photoImage.image = UIImage(named: "KwaliteitenQuiz")
             
         case"Foto challenge" : descriptionLabel.text = "Maak een foto die jouw droom omschrijft. Je mag items gebruiken, mensen, omgevingen, wees creatief!. Vraag eens aan anderen of ze je droom erin herkennen."
-        photoImage.image = UIImage(named: "FotoChallenge")
+//        photoImage.image = UIImage(named: "FotoChallenge")
         saveButton.tintColor = UIColor.gray
             
         case "Welja, geen nee" :
             descriptionLabel.text = "NEEgativiteit staat tussen jou en je droom in. Hoe los je dit op? Door gewoon lekker ja te zeggen, word je blij van. Bonus: Het brengt je droom een beetje (of heel veel) dichterbij. Wil je toch?"
-            photoImage.image = UIImage(named: "No no")
+//            photoImage.image = UIImage(named: "No no")
             
         case "Ik heb een droom en ik neem mee":
             descriptionLabel.text = "Ik ga op reis en ik neem mee, maar dan met dromen! Wat heb jij nodig om jouw droom te kunnen realiseren? "
-            photoImage.image = UIImage(named: "Ik heb een droom")
+//            photoImage.image = UIImage(named: "Ik heb een droom")
             startButton.isEnabled = false
             startButton.isHidden = true
             
         case "Dilemma's":
             descriptionLabel.text = "Heb je moeite met keuzes maken? Dan heb je deze dilemma’s nog niet gelezen. Oefen hier eens mee, dan worden je eigen dilemma’s makkelijker op te lossen!"
-            photoImage.image = UIImage(named: "Dilemma")
+//            photoImage.image = UIImage(named: "Dilemma")
             
         case "Raad de tekening":
             descriptionLabel.text = "Teken een element dat betrekking heeft op jouw droom. Laat tijdens het tekenen je vrienden raden wat het wordt!"
-            photoImage.image = UIImage(named: "Draw something")
+//            photoImage.image = UIImage(named: "Draw something")
             
             default: break
         }
