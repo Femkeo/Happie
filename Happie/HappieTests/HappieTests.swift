65abedaf20e4d5b806e62d184c55688f71bd86f6
Femke Offringa <foffringa@mobiquityinc.com>
2019-03-18 17:08:21 +0100

    added testfiles

--- Happie/HappieTests/HappieTests.swift
+++ Happie/HappieTests/HappieTests.swift
@@ -7,8 +7,13 @@
 //
 
 import XCTest
+@testable import Happie
 
 class HappieTests: XCTestCase {
+    
+    
+    let gameData = GameData()
+    let userData = UserData()
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -17,12 +22,56 @@ class HappieTests: XCTestCase {
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func testUnlockingNextLevel(){
+        
     }
+    
+    func testIfDreamIsBeginSaved(){
+        userData.Data = [String: Any]()
+        userData.initializeStartingUserData()
+        let result : String = userData.result["hair"] as! String
+        XCTAssertEqual(result,
+                       "Hair1", "The result should be Hair1, but it is \(result)")
+    }
+    
+    func testIfDataIsRetreived(){
+        gameData.creatingGames()
+        let result = gameData.Games.keys.first
+        
+        XCTAssertEqual(result, "Categories",
+                       "The result should be 'Categpories' but is \(result ?? "not found")")
+    }
+    
+    func testIfScoreHasBeenSaved(){
+        
+    }
+    
+    func testSavingOfUserData(){
+        
+    }
+    
+    //NoMonitor
+    func testNoMonitorTimer(){
+        
+    }
+    
+    func testNoMonitorAudio(){
+        
+    }
+    
+    func testPropertyReader(){
+        
+    }
+    
+    
+    
+//
+//    func testPerformanceExample() {
+//        // This is an example of a performance test case.
+//        self.measure {
+//            // Put the code you want to measure the time of here.
+//        }
+//    }
 
 }
--- Happie/HappieUITests/HappieUITests.swift
+++ Happie/HappieUITests/HappieUITests.swift
@@ -7,8 +7,11 @@
 //
 
 import XCTest
+@testable import Happie
 
 class HappieUITests: XCTestCase {
+    
+    let progressStatusPage = ViewController()
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -22,13 +25,24 @@ class HappieUITests: XCTestCase {
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
 
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    func testProgressBarIncrease(){
+        // should I?
     }
-
-    func testExample() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    func testIsUsersAvatar(){
+        // Should I?
     }
+    
+    func testIfdrawingIsPossible(){
+        
+    }
+    
+    func testIfGameExplenationpageIsShown(){
+        
+    }
+    
+    
+    
+    
 
 }
